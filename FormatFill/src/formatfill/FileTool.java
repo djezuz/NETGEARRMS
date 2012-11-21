0185906e04d1ebdb8c76dddb279428a43dc7da95
chensi <343476807@qq.com>
2012-11-21 15:32:34 +0800

    formFill

---
+++ FormatFill/src/formatfill/FileTool.java
@@ -0,0 +1,41 @@
+package formatfill;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Date;
+import java.util.Timer;
+
+public class FileTool {
+	public static StringBuffer getStringBuffer(String fileName) {
+		StringBuffer str = new StringBuffer();
+		BufferedReader br;
+		try {
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(
+					new File(fileName)),"UTF-8"));
+			String s = "";
+			while ((s = br.readLine()) != null) {
+				str.append(s+"\n");
+			}
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return str;
+	}
+
+	public static void execute() {
+		Timer timer = new Timer();
+		MyTimer myTimeTask = new MyTimer();
+		Date date = new Date();
+		long timestamp = 1000 * 10*60;
+		timer.schedule(myTimeTask, date, timestamp);
+	}
+	public static void main(String[]args){
+		System.out.println(getStringBuffer("d://Combined.str").toString());
+	}
+
+}
---
+++ FormatFill/src/formatfill/MyTimer.java
@@ -0,0 +1,49 @@
+package formatfill;
+import java.util.TimerTask;
+import org.apache.axis.client.Call;
+import org.apache.axis.client.Service;
+
+public class MyTimer extends TimerTask {
+	String last="";
+	public void run() {
+		StringBuffer list=new StringBuffer();
+		StringBuffer str = FileTool.getStringBuffer(Activator.getRoot()+"File/status.log-old");
+		String value=str.toString().trim();
+		String tmp=value.substring(value.lastIndexOf("\n")+1);
+		if(!last.equals("")&&value.contains(last)){
+			if(!last.equals(tmp)){
+				value=value.substring(value.indexOf(last)+last.length()).trim();
+			}else{
+				return;
+			}
+		}
+		last=tmp;
+		for(String s:Activator.list){
+			if(value.contains(s)){
+				String s0=value.substring(value.indexOf(s));
+				s0=s0.substring(0,s0.indexOf("\n"));
+				String s1=value.substring(0,value.indexOf(s));
+				s1=s1.substring(s1.lastIndexOf("\n")+1);
+				list.append(s1+s0+"\n");
+			}
+		}
+		getCall(list.toString());
+	}
+	
+	public static Object getCall(String value){
+		String url="http://localhost:8080/server/services/login";
+		Service service=new Service();
+		Call call;
+		String rec="";
+		try{
+			call=(Call)service.createCall();
+			call.setTargetEndpointAddress(new java.net.URL(url));
+			call.setOperationName("success");
+			rec=(String) call.invoke(new Object[]{value,"","","",""});
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		System.out.println(rec);
+		return rec;
+	}
+}
