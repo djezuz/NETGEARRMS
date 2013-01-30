3af6bb34acbea3ae22951f7248bf5ed29fb0eef7
liuhong <hong.liu@dragonflow.com>
2013-01-30 12:01:59 +0800

    解析文件

--- _ReadyNAS_RMS/src/_readynas_rms/FileTool.java
+++ _ReadyNAS_RMS/src/_readynas_rms/FileTool.java
@@ -6,10 +6,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
 import java.util.Timer;
 
 public class FileTool {
@@ -41,70 +37,7 @@ public class FileTool {
 		long timestamp = 1000*60;
 		timer.schedule(myTimeTask, date, timestamp);
 	}
-	public static void main(String[]args){
-		//System.out.println(getStringBuffer("d://Combined.str").toString());
-		System.out.println(getS_no());
-	}
 	
-	public static String getS_no(){
-		String s_no = "";
-		String sessionId="";
-		String jsessionId="";
-		String DeviceConfig="";
-		
-		String key="";
-		String value="";
-		String result="";
-		String name="";
-		String model="";
-		//第一次发送请求
-		Map<String,String> map = new HashMap<String,String>();
-		map.put("init", "<init type=\"ui\" fcmb=\"true\"/>");
-		Set set=map.keySet();
-		for(Iterator it=set.iterator(); it.hasNext();){
-			   key  =   (String)   it.next();
-			   value=   map.get(key);
-			  result=SOAPClient4XG.remoteRun(key,value);
-			  System.out.println("第一次返回的结果:"+result);
-			  if(!(result.equals(""))&&!(result.equals("<authenticate result=\"401\" description=\"Unauthorized\"/>"))){
-				  String str=result.substring(0,result.lastIndexOf(">")+1);
-				  sessionId=result.substring(result.lastIndexOf(">")+1);
-				  
-				  sessionId.substring(0, sessionId.lastIndexOf("=")+1);
-				  jsessionId=sessionId.substring(sessionId.lastIndexOf("=")+1);
-				  
-				  String []a=jsessionId.split(";");
-				  DeviceConfig=a[0];
-			  }  
-		} 
-		//第二次发送请求
-		if(!(result.equals(""))&&!(result.equals("<authenticate result=\"401\" description=\"Unauthorized\"/>"))){
-			String str=result.substring(result.indexOf("name=")+5);
-			name=  str.substring(str.indexOf('"')+1, str.indexOf('"', 1));	
-		}
-		String a="send?n="+name;
-		String fcml="<fcml to=\"router@portal\" from=\""+name+"@portal\" _tracer=\"8\"><get/></fcml>";
-		result=SOAPClient4XG.connectionRun(sessionId, a, fcml);
-		if(result.equals("")){
-			result="200 ok";
-		}
-		System.out.println("第二次返回的结果:"+result);
-		
-		//第三次发送请求并获取model以及model的序列号
-		String a1="receive?n="+name;
-		String fcml1="<DeviceConfig.ConfigurationStarted=\""+DeviceConfig+"\"/>";
-		result=SOAPClient4XG.connectionRun(sessionId, a1, fcml1);
-		System.out.println("第三次返回的结果:"+result);
-		if(!(result.equals(""))&&!(result.equals("<authenticate result=\"401\" description=\"Unauthorized\"/>"))){
-			String str=result.substring(result.indexOf("model=")+6);
-			model=str.substring(str.indexOf('"')+1, str.indexOf('"', 1));
-			String string=result.substring(result.indexOf("serial=")+7);
-			String number=string.substring(string.indexOf('"')+1, string.indexOf('"', 1));
-			System.out.println("model:"+model);
-			System.out.println("设备序列号"+number);
-			s_no = number;
-		}
-		return s_no;
-	}
+	
 
 }
--- _ReadyNAS_RMS/src/_readynas_rms/MyTimer.java
+++ _ReadyNAS_RMS/src/_readynas_rms/MyTimer.java
@@ -1,9 +1,8 @@
 package _readynas_rms;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.TimerTask;
 
 import org.apache.axis.client.Call;
@@ -13,49 +12,22 @@ import org.apache.axis.client.Service;
 
 public class MyTimer extends TimerTask {
 	
-	
+	public static Map<String, String> map = new HashMap<String, String>();
+	public static Set<String> list = new HashSet<String>();
+	public String last="";
 	public static final String PLUGIN_ID = "ReadyNAS_RMS";
-	public static ArrayList<String> last=new ArrayList<String>();
 	public void run() {
-		StringBuffer list=new StringBuffer();
 		StringBuffer str = FileTool.getStringBuffer("/var/log/frontview/status.log-old");
-		/*System.out.println("------------------------------------");
-		String value=str.toString().trim();
-		String tmp=value.substring(value.lastIndexOf("\n")+1);
-		System.out.println("----tmp----"+tmp);
-		if(!last.equals("")&&value.contains(last)){
-			if(!last.equals(tmp)){
-				value=value.substring(value.indexOf(last)+last.length()).trim();
-			}else{
-				return;
-			}
-		}
-		last=tmp;
-		System.out.println("-----------Activator.list-"+Activator.list.size());
-		for(String s:Activator.list){
-			System.out.println(s+"............................s"+s);
-			System.out.println(value.contains(s));
-			System.out.println(value);
-			System.out.println("++++++++++++++++++++++++++");
-			System.out.println(s);
-			if(value.contains(s)){
-				
-				String s0=value.substring(value.indexOf(s));
-				s0=s0.substring(0,s0.indexOf("\n"));
-				String s1=value.substring(0,value.indexOf(s));
-				s1=s1.substring(s1.lastIndexOf("\n")+1);
-				list.append(s1+s0+"\n");
-			}
-		}
-		System.out.println(list.toString()+"..................soap--");
-		getCall(list.toString());*/
 		getinputstream(Activator.str0, Activator.str1,str.toString());
 	}
 	
-	public  static String getinputstream(String str0,String str1,String str){
+	public  String getinputstream(String str0,String str1,String str){
 		
-		String serial_no = FileTool.getS_no();
-		System.out.println("调用WSDL:"+serial_no);
+		String serial_no ="2UM11AEF00170";
+		
+		String sussessvalue=parse(str0,str1,str).toString();
+		
+		System.out.println("call start:"+serial_no);
 		String url="http://192.168.9.61:8080/server/services/login";
 		 
 		Service service=new Service();
@@ -65,62 +37,104 @@ public class MyTimer extends TimerTask {
 			call=(Call)service.createCall();
 			call.setTargetEndpointAddress(new java.net.URL(url));
 			call.setOperationName("success1");
-			String laststr="";
-			if(last.size()>0){
-				laststr=last.get(0);
-			}
-			String arrlist=(String) call.invoke(new Object[]{str0,str1,str,serial_no,laststr});
-			last.add(0, arrlist);
+			String arrlist=(String) call.invoke(new Object[]{serial_no,sussessvalue});
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-		System.out.println("~~~~~调用完成:"+rec);
+		System.out.println("~~~~~call stop:"+rec);
 		return rec;
 		
 	}
 	
-	
-	
-	public static Object getCall(String value){
-		String serial_no = FileTool.getS_no();
-		System.out.println(serial_no);
-		String url="http://192.168.9.61:8080/server/services/login";
-		 			 
-		//StringBuffer str=FileTool.getStringBuffer(getRoot()+"META-INF/config.ini");
-		/*StringBuffer str=new StringBuffer("http://192.168.9.61:8080/server/services/login");
-		System.out.println(str);
-		String [] s=str.toString().split("=");
-		String  url=s[1];
-		System.out.println(url);*/
-		Service service=new Service();
-		Call call;
-		String rec="";
-		try{
-			call=(Call)service.createCall();
-			call.setTargetEndpointAddress(new java.net.URL(url));
-			call.setOperationName("success");
-			rec=(String) call.invoke(new Object[]{value,serial_no,"","",""});
-		}catch(Exception e){
-			e.printStackTrace();
+	public  StringBuffer parse(String str0, String str1, String str) {
+
+		String s = str0;
+		String[] keys = { "S_SMART_ERRORS_REALLOCATED_SECTOR_MESG=",
+				"S_SMART_ERRORS_ATA_ERROR_MESG=", "S_SMART_STATUS_FAILED=" };
+		for (String key : keys) {
+
+			if (s.contains(key)) {
+				String value = s.substring(s.indexOf(key));
+				value = value.substring(0, value.indexOf("\n"));
+				if (value.contains("=")) {
+					if (map.get(value.substring(0, value.indexOf("="))) == null) {
+						map.put(value.substring(0, value.indexOf("=")), value
+								.substring(value.lastIndexOf("=") + 1));
+					}
+					if (!list.contains(value.substring(
+							value.lastIndexOf("=") + 2, value.length() - 1))) {
+						list.add(value.substring(value.lastIndexOf("=") + 2,
+								value.length() - 1));
+					}
+
+				}
+			}
 		}
-		System.out.println(rec);
-		return rec;
+		String[] keys_1 = { "DISK_OVERTEMP_BODY::::", "POWER_OUT_OF_SPEC::::",
+				"FAN + HAS_FAILED::::" };
+		s = str1.toString();
+		for (String key : keys_1) {
+			System.out.println(key);
+			System.out.println(s);
+			if (s.contains(key)) {
+				String value = s.substring(s.indexOf(key));
+				value = value.substring(0, value.indexOf("\n"));
+				if (value.contains("::::")) {
+					if (map.get(value.substring(0, value.indexOf("::::"))) == null) {
+						map.put(value.substring(0, value.indexOf("::::")),
+								value.substring(value.lastIndexOf("::::") + 4));
+					}
+					if (!list.contains(value.substring(value
+							.lastIndexOf("::::") + 4))) {
+						list
+								.add(value
+										.substring(value.lastIndexOf("::::") + 4));
+					}
+				}
+			}
+		}
+
+		StringBuffer list1 = new StringBuffer();
+		String value = str.toString().trim();
+		String tmp = value.substring(value.lastIndexOf("\n") + 1);
+		
+//		if ("".equals(this.last)) {
+//			String logvalue = mydesign.queryLast();
+//			if (logvalue != null && !"".equals(logvalue)) {
+//				this.last = logvalue;
+//			}
+//		}
+//		if (!this.last.equals(tmp)) {
+//			mydesign.saveTmpvalue(tmp);
+//		}
+		// save tmp
+		if (!last.equals("") && value.contains(last)) {
+			value = value.substring(value.lastIndexOf(last) + last.length())
+					.trim();
+		}
+		this.last = tmp;
+
+		System.out.println("~~~   list.size" + list.size() + "~~~~~log value=:"
+				+ value);
+
+		 String[] str_vlaue = value.split("\n");
+		if (str_vlaue != null && str_vlaue.length > 0) {
+			for (String ss : str_vlaue) {
+				if (ss != null && !"".equals(ss)) {
+					for (String s2 : list) {
+						System.out.println("!!!!list----" + s2);
+						if (ss.contains(s2)) {
+//							System.out.println("success one value" + ss);
+							list1.append(ss + "\n");
+						}
+					}
+				}
+			}
+		}
+//		System.out.println("success all value："+list1);
+		return list1;
+
 	}
 	
 	
-//	public static String getRoot(){
-//		String path = null;
-//		try {
-//			path = Activator.getContext().getBundle().getEntry("").getPath();
-//
-//			// path = FileLocator.toFileURL(
-//			// Platform.getBundle(PLUGIN_ID).getEntry("")).getPath();
-//			// path=Platform.getBundle(PLUGIN_ID).getLocation();
-//			path = path.substring(path.indexOf("/") + 1, path.length());
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		System.out.println(path);
-//		return path;
-//	} 
 }
